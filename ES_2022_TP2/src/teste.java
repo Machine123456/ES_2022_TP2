b7e680d2a3fa40a2d538d7951bc1af8e198e94b9
andre <andreffalao@gmail.com>
2022-05-22 05:25:19 +0100

    Commit depois de add a tag

--- ES_2022_TP2/src/teste.java
+++ ES_2022_TP2/src/teste.java
@@ -5,6 +5,7 @@ public class teste {
 	public static void main(String[] args) {
 		System.out.println("Sou um teste? :)");
 		System.out.println("Alterado no Github");
+		System.out.println("commit com tag?");
 	}
 	
 	
